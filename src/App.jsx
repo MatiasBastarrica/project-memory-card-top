bf16c9e4974fc3dfa2076d1bbfdc718b9e55800e
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-07-27 11:39:52 -0300

    Create a "Card section" component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
+import { CardsSection } from "./components/CardsSection.jsx";
 
 function App() {
   return (
     <>
-      <h1>Project: Memory Card</h1>
+      <h1>Project: Memory Card Zelda edition</h1>
+      <CardsSection></CardsSection>
     </>
   );
 }
